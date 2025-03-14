dc92452af3623f2463e52311977bab80464c16c8
dgagan2 <andgag@duck.com>
2025-03-13 20:51:09 -0500

    feat: Se establecen los iconos de cada item

--- src/components/navbar/DockNavbar.tsx
+++ src/components/navbar/DockNavbar.tsx
@@ -1,40 +1,43 @@
-import "./navbar.css";
-import iconPrueba from "../../assets/icons/smile.png";
 import projectIcon from "../../assets/icons/proyectos.png";
+import aboutMeIcon from "../../assets/icons/aboutMe.png";
+import skillsIcon from "../../assets/icons/skills.png";
+import homeIcon from "../../assets/icons/home.png";
+import "./navbar.css";
+
 const DockNavbar = () => {
     return (
-        <nav className="menu">
-            <ul className="menu__container">
-                <li className="menu-item">
-                    <a href="#">
+        <nav className="dock__navbar">
+            <ul className="dock__navbar__menu">
+                <li className="dock__navbar__menu__item">
+                    <a href="/">
                         <span>
-                            menu1
+                            Home
                         </span>
-                        <img src={iconPrueba} alt="" />
+                        <img src={homeIcon} alt="Icono pagina principal" />
                     </a>
                 </li>
-                <li className="menu-item">
-                    <a href="">
+                <li className="dock__navbar__menu__item">
+                    <a href="#proyectos">
                         <span>
                             Proyectos
                         </span>
-                        <img src={projectIcon} alt="" />
+                        <img src={projectIcon} alt="Icono de proyectos" />
                     </a>
                 </li>
-                <li className="menu-item">
-                    <a href="">
+                <li className="dock__navbar__menu__item">
+                    <a href="#aboutMe">
                         <span>
                             Sobre Mi
                         </span>
-                        <img src={iconPrueba} alt="" />
+                        <img src={aboutMeIcon} alt="Icono información acerca de mi" />
                     </a>
                 </li>
-                <li className="menu-item">
-                    <a href="">
+                <li className="dock__navbar__menu__item">
+                    <a href="#skills">
                         <span>
-                            Skills
+                            Habilidades
                         </span>
-                        <img src={iconPrueba} alt="" />
+                        <img src={skillsIcon} alt="Icono de habilidades" />
                     </a>
                 </li>
             </ul>
