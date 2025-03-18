67d6d8ae88f8c7c75c3d7949e766a17b163b7e27
dgagan2 <andgag@duck.com>
2025-03-17 23:41:44 -0500

    feat: Se crea un componente de flecha, se usa al hacer hover sobre ciertas imagenes

---
+++ src/components/iconoFlecha/IconoFlecha.tsx
@@ -0,0 +1,12 @@
+import icon from "../../assets/icons/clic.png"
+import "./iconFlecha.css"
+
+const IconoFlecha = () => {
+    return (
+        <span className='icono-flecha-hover'>
+            <img src={icon} alt="icono de flecha" />
+        </span>
+    )
+}
+
+export default IconoFlecha;
