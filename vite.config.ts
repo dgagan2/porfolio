bef4c55a4533f10fc06e60202e875280582b8af4
dgagan2 <andgag@duck.com>
2025-04-12 16:23:57 -0500

    fix: Se ajusta archivo vite.config.js para solucionar problema de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
   },
 })
+
